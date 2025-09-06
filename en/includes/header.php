d32baf605074eade5965339605e52aeb6e479dcf
hugobertagna <h.bertagna118@gmail.com>
2025-09-06 18:28:28 +0200

    favicon

--- en/includes/header.php
+++ en/includes/header.php
@@ -5,12 +5,12 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>Portfolio</title>
+  <title>Hugo BERTAGNA</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
   <link href="/assets/css/style.css" rel="stylesheet">
-  <link rel="icon" type="image/png" href="/assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
 
 </head>
 <body>
